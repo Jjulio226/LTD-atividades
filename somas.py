d627cd40fd59f310bf759a1b264e8ccd423e171f
JoséJúlio <137164072+Jjulio226@users.noreply.github.com>
2025-04-08 15:16:42 -0300

    Realizar todas as operações matemática.

---
+++ somas.py
@@ -0,0 +1,16 @@
+n1 = int(input("Insira o primero numero:" ))
+n2 = int(input("Insira o proximo numero: "))
+
+soma = n1+n2
+multi = n1*n2
+sub = n1-n2
+div = n1%n2
+print("____________________________________________________________")
+print(f"O resultado da soma de {n1} + {n2} é: {soma}")
+print(".............................................................")
+print(f"O resultado da multiplicação entre {n1} * {n2} é: {multi}")
+print(".............................................................")
+print(f"O resultado da subtção entre {n1} - {n2} é: {sub}")
+print(".............................................................")
+print(f"O resultado da divisão entre {n1} % {n2} é: {div}")
+print("____________________________________________________________")
