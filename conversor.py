2c95fed928b49f7a360828c5447a5d62450690d9
JoséJúlio <137164072+Jjulio226@users.noreply.github.com>
2025-04-08 15:27:10 -0300

    Converte de Celsius para Fahrenheit.

--- conversor.py
+++ conversor.py
@@ -1,6 +1,6 @@
-print("---------------------------------------------")
+print("----------------------------------------------")
 print("Conversor de °C para °F")
-print("---------------------------------------------")
+print("----------------------------------------------")
 temp=float(input("Coloque aqui a temperatura em °C: "))
 farh = (temp) * 1.8 + 32 
 print(f" A conversão de {temp}°C é: {farh}°F ")
