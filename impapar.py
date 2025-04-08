c26339917cb62f2c4988c529f2c890cda2193541
JoséJúlio <137164072+Jjulio226@users.noreply.github.com>
2025-04-08 15:15:06 -0300

    Aprovado!

--- impapar.py
+++ impapar.py
@@ -1,5 +1,12 @@
-print("Bem vindo ao verificador de par ou impa")
-
-n1= int(input("Digite um número: "))
-n2= int(input("Digite outro número: "))
+print("Verificador de numeros, par ou impa.")
+n1= int(input("Insira um número: "))
+print("_____________________________")
+n2= int(input("Insira outro numero: "))
+print("-----------------------------")
+soma = n1 + n2
+if soma %2 == 0:
+    print(f"O numero {soma} é par.")
+else:
+    print(f"{soma} é impar.")
+print("---------------------------")
 
