105fbbd301c1e0495d0186f6c1180b3714df4f66
JoséJúlio <137164072+Jjulio226@users.noreply.github.com>
2025-04-08 15:24:02 -0300

    Create conversor.py

---
+++ conversor.py
@@ -0,0 +1,7 @@
+print("---------------------------------------------")
+print("Conversor de °C para °F")
+print("---------------------------------------------")
+temp=float(input("Coloque aqui a temperatura em °C: "))
+farh = (temp) * 1.8 + 32 
+print(f" A conversão de {temp}°C é: {farh}°F ")
+print("---------------------------------------------")
