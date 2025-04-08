f6b73f6f8784fd03f38afa905c527f27277a5eb2
JoséJúlio <137164072+Jjulio226@users.noreply.github.com>
2025-04-08 16:31:33 -0300

    Update imc.py

--- imc.py
+++ imc.py
@@ -1,20 +1,20 @@
 idade = int(input("Digite sua idade: "))
 altura = float(input("Digite sua altura :"))
-peso = float(input("Digite seu peso: "))
+peso = float(input("Digite seu peso em kg: "))
 
 imc = peso % (altura * altura)
 
 print(f"seu resultado é: {imc}")
 
-if imc<18.5:
-    print(f"{imc} pode ser considerado magreza.")
-elif imc >= 18.5 and 24.9:
-    print(f"{imc} seu peso está normal")
-elif imc >= 25 and 29.9:
-    print(f"{imc} está com sobrepeso")
-elif imc >= 30 and 34.9:
-    print(f"{imc} é considerado obesidade grau I")
-elif imc >= 35 and 39.9:
-    print(f"{imc} é considerado obesidade grau II")
-elif imc >= 40:
-    print(f"{imc} obesidade grau III")
+if imc < 18.5:
+   print(f"Classificação: Magreza.")
+elif 18.5 <= imc <= 24.9:
+    print(f"Classificação: Peso normal.")
+elif 25 <= imc <= 29.9:
+    print(f"Classificação: Sobrepeso.")
+elif 30 <= imc <= 34.9:
+     print(f"Classificação: Obesidade grau I.")
+elif 35 <= imc <= 39.9:
+     print(f"Classificação: Obesidade grau II.")
+else: # Para IMC >= 40
+     print(f"Classificação: Obesidade grau III.")
